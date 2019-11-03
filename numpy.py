03f0eea54efa088a1e63817fb1f584f9a50a9023
Jan <jas.kocurek@wp.pl>
2019-11-03 13:17:39 +0100

    first commit 2

--- numpy.py
+++
@@ -1,2 +0,0 @@
-'''Learnin to use numpy
-Numpy is used to work on multi-dmientional array and is much faster than lists.'''
---
+++ numpy_1.py
@@ -0,0 +1,6 @@
+'''Learnin to use numpy.
+
+Numpy is used to work on multi-dmientional array and is much faster than lists.
+Its faster for a number of reasons like all the elements are the same type or each element
+doesnt have properties like size or num of references. The numpy arrays also use
+Contiguous memory (the pointers are tidly oredered).'''
